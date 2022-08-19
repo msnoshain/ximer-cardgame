4fba6780e4eeaac1f0f8661f3f301c4b3f933c3d
Slash_Bean <1178890320@qq.com>
2022-08-20 02:07:03 +0800

    1st commit

---
+++ Ximer.CardGame/Ximer.CardGame/Match.cs
@@ -0,0 +1,18 @@
+﻿namespace Ximer.CardGame.Core;
+
+public abstract class Match
+{
+    public long MatchID { get; set; }
+
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public abstract string MatchType { get; set; }
+
+    public abstract IEnumerable<Player> Players { get; set; }
+
+    public abstract IMatchController MatchController { get; set; }
+
+    public abstract IEnumerable<Card> CardPool { get; set; }
+}
